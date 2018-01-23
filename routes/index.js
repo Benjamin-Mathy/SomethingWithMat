1c6b9091e30e9cb8c19e6c4e6f99d27cde98b999
MattBabak <matthieu.babak@gmail.com>
2018-01-24 00:29:15 +0100

    Register form.jade and .js Bad reception No message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'xD' });
+  res.render('index', { title: 'xD' , suite:'ca va etre bien' });
+});
+router.get('/register', function(req, res, next) {
+    res.render('register', { title: 'Page d\'inscription' });
 });
 
+router.post('/register', function(req,res,next){
+    var login= res.get('login');
+    var password= res.get('password');
+    console.log ( 'Votre Login :' + login + ' et votre mdp :' + password + ' . ')
+});
 module.exports = router;
---
+++ routes/register.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/register', function(req, res, next) {
+    res.render('register', { title: 'Page d\'inscription' });
+});
+
+router.post('/register', function(req,res,next){
+var login= res.get('login');
+var password= res.get('password');
+console.log ( 'Votre Login :' + login + ' et votre mdp :' + password + ' . ')
+});
+
+module.exports = router;
